29a25c19752cf1248d297cdf125541aa399f3a73
Curtis Lee <midprogram@curtiss-macbook-pro.local>
2019-01-20 12:28:53 -0500

    Initial Commit

---
+++ calcApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calcApp
+//
+//  Created by Curtis Lee on 1/20/19.
+//  Copyright © 2019 clee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
